120728bee709b9db6d9883fc4621e20e5579832a
Adam <adamniemiec862@gmail.com>
2023-03-06 20:46:36 +0100

    Display legend

--- lab1/plotCreator.py
+++ lab1/plotCreator.py
@@ -6,9 +6,9 @@ node2 = pd.read_csv("node2_results.csv", sep=';', usecols=["bytes", "throughput"
 node1_sync = pd.read_csv("node1_sync_results.csv", sep=';', usecols=["bytes", "throughput"])
 node2_sync = pd.read_csv("node2_sync_results.csv", sep=';', usecols=["bytes", "throughput"])
 
-plt.title('2 nodes communication')
-plt.xlabel('message length [B]')
-plt.ylabel('throughput [Mbit/s]')
+plt.title('Throughput for 2 nodes communication')
+plt.xlabel('Message length [B]')
+plt.ylabel('Throughput [Mbit/s]')
 plt.grid(color='grey', linestyle='-', linewidth=1)
 
 # plt.plot(node1.bytes, node1.throughput, label="MPI_Send")
@@ -16,4 +16,5 @@ plt.grid(color='grey', linestyle='-', linewidth=1)
 
 plt.plot(node2.bytes, node2.throughput, label="MPI_Send")
 plt.plot(node2_sync.bytes, node2_sync.throughput, label="MPI_Ssend", color='orange')
+plt.legend()
 plt.show()
