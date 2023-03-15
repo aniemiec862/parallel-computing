1202dcc2c38647cf41433dfa12a88fcec6417138
Adam <adamniemiec862@gmail.com>
2023-03-15 23:45:16 +0100

    Final send_recv.c version

--- MPI/p2p/plotCreator.py
+++ MPI/p2p/plotCreator.py
@@ -6,16 +6,16 @@ node2 = pd.read_csv("node2_results.csv", sep=';', usecols=["b", "t"])
 node1_sync = pd.read_csv("node1_sync_results.csv", sep=';', usecols=["b", "t"])
 node2_sync = pd.read_csv("node2_sync_results.csv", sep=';', usecols=["b", "t"])
 
-plt.title('Throughput for 1 node communication')
+plt.title('Throughput for 2 nodes communication')
 plt.xlabel('Message length [B]')
 plt.ylabel('Throughput [Mbit/s]')
 plt.grid(color='grey', linestyle='-', linewidth=1)
 
-plt.plot(node1.b, node1.t, label="MPI_Send")
-# plt.scatter(node1_sync.b, node1_sync.t, label="MPI_Ssend", color='orange')
+# plt.plot(node1.b, node1.t, label="MPI_Send")
+# plt.plot(node1_sync.b, node1_sync.t, label="MPI_Ssend", color='orange')
 
-# plt.scatter(node2.b, node2.t, label="MPI_Send")
-# plt.scatter(node2_sync.b, node2_sync.t, label="MPI_Ssend", color='orange')
+# plt.plot(node2.b, node2.t, label="MPI_Send")
+plt.plot(node2_sync.b, node2_sync.t, label="MPI_Ssend", color='orange')
 
 plt.legend()
 plt.show()
--- MPI/p2p/send_recv.c
+++ MPI/p2p/send_recv.c
@@ -1,46 +1,67 @@
-#include "definitions.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <mpi.h>
 
-double sender(int msg_size);
+#define TAG 0
 
-double sender(int msg_size) {
-    char *buff = malloc(msg_size);
-    int i;
-    double start = MPI_Wtime();
-    for (i = 0; i < N; i++) {
-        MPI_Send(buff, msg_size, MPI_BYTE, RECEIVER, 0, MPI_COMM_WORLD);
-        MPI_Recv(buff, msg_size, MPI_BYTE, RECEIVER, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    }
-    return MPI_Wtime() - start;
-}
-
-void receiver(int msg_size) {
-    char *buff = malloc(msg_size);
-    int i;
-    for (i = 0; i < N; i++) {
-        MPI_Recv(buff, msg_size, MPI_BYTE, SENDER, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        MPI_Send(buff, msg_size, MPI_BYTE, SENDER, 0, MPI_COMM_WORLD);
-    }
-}
-
-void test(long msg_size, int rank) {
-    if (rank == SENDER) {
-        double time = sender(msg_size);
-        printf("%ld;%f  \n", msg_size, N*msg_size/(64*1024*time));
-    } else if (rank == RECEIVER) {
-        receiver(msg_size);
-    }
+int compare_doubles(const void* a, const void* b) {
+    double x = *(double*)a;
+    double y = *(double*)b;
+    return (x < y) ? -1 : ((x > y) ? 1 : 0);
 }
 
 int main(int argc, char *argv[]) {
-
     MPI_Init(&argc, &argv);
 
-    int rank;
+    int rank, size, i;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    long msg_size;
-    char *eptr;
-     msg_size = strtol(argv[1], &eptr, 20);
-    test(msg_size, rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    int test_count = 100;
+    int bytes_to_send = atoi(argv[1]);
+    char* message = malloc(bytes_to_send + 1);
+    memset(message, 'A', bytes_to_send);
+    message[bytes_to_send] = '\0';
+
+    double start, stop, time, summed_time = 0.0;
+    double* res = malloc(sizeof(double) * test_count);
+
+    for (i = 0; i < test_count; i++) {
+        MPI_Barrier(MPI_COMM_WORLD);
+        start = MPI_Wtime();
+
+        if (rank == 0) {
+            MPI_Send(message, bytes_to_send, MPI_CHAR, 1, TAG, MPI_COMM_WORLD);
+            MPI_Recv(message, bytes_to_send, MPI_CHAR, 1, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        } else if (rank == 1) {
+            MPI_Recv(message, bytes_to_send, MPI_CHAR, 0, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Send(message, bytes_to_send, MPI_CHAR, 0, TAG, MPI_COMM_WORLD);
+        }
+
+        stop = MPI_Wtime();
+        time = (stop - start) / 2;
+        res[i] = time;
+        summed_time += time;
+    }
+
+    if (rank == 0) {
+        double result = summed_time / test_count;
+        double median;
+        int middle = test_count / 2;
+
+        // Sort the results array
+        qsort(res, test_count, sizeof(double), compare_doubles);
+
+        if (test_count % 2 == 0) {
+            median = (res[middle - 1] + res[middle]) / 2;
+        } else {
+            median = res[middle];
+        }
+
+        double speed_in_mega_bits = (bytes_to_send * 8 / median) / 1000000;
+        printf("%d;%f\n", bytes_to_send, speed_in_mega_bits);
+    }
 
     MPI_Finalize();
     return 0;
