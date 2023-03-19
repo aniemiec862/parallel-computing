f040a338a903644e21ac7c25e7ca4e43e3164770
Adam <adamniemiec862@gmail.com>
2023-03-19 17:09:26 +0100

    Update ares scripts

--- MPI/monte_carlo/const_size_per_thread.sh
+++ MPI/monte_carlo/const_size_per_thread.sh
@@ -1,20 +1,20 @@
 #!/bin/bash -l
 #SBATCH --nodes 1
 #SBATCH --ntasks 12
-#SBATCH --time=12:00:00
+#SBATCH --time=06:00:00
 #SBATCH --partition=plgrid
 #SBATCH --account=plgmpr23-cpu
 
 module add scipy-bundle/2021.10-foss-2021b
 
-for pointsPerThread in 62500 1767766 50000000
+for pointsPerThread in 10000 3535533 1250000000
 do
     for threads in {1..12}
     do
         for i in {1..10}
         do
             totalPoints=$((pointsPerThread * threads))
-            mpiexec -np $threads ./pi.py $totalPoints
+            mpiexec -np $threads ./mc.py $totalPoints
         done
     done
 done
