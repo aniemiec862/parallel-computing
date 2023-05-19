89dde1ef42b3fccdec9d718eacd11648771f3f52
Adam <adamniemiec862@gmail.com>
2023-05-19 12:39:11 +0200

    Init MapReduce

--- MapReduce/seq_word_count.py
+++ MapReduce/seq_word_count.py
@@ -1,19 +1,30 @@
 import sys
+import time
+import collections
 
+def process_file(file_path, block_size=4096):
+    with open(file_path, 'r', encoding='latin-1') as file:
+        while True:
+            block = file.read(block_size)
+            if not block:
+                break
+            yield block.split()
 def word_count(file_name):
-    word_count_dict = {}
-    with open(file_name, 'r', encoding='latin-1') as file:
-        for line in file:
-            words = line.split()
-            for word in words:
-                if word in word_count_dict:
-                    word_count_dict[word] += 1
-                else:
-                    word_count_dict[word] = 1
+    counter = collections.Counter()
+    lines = process_file(file_name)
+    for words in lines:
+        counter.update(words)
 
-    for word, count in word_count_dict.items():
+    for word, count in counter.items():
         print(f"{word} : {count}")
 
 
-file_name = sys.argv[1]
-word_counts = word_count(file_name)
+experiment_id = sys.argv[1]
+file_size = sys.argv[2]
+file_name = "gutenberg-" + file_size + "GB.txt"
+start = time.time()
+word_count(file_name)
+result = time.time() - start
+
+with open('result.txt', 'a') as f:
+    print(str(experiment_id) + ";" + file_size + ";" + str(result), file=f)
