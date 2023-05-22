a17bbafd7f01b7a051d1f75e9fd60e80f1cc70b0
Adam <adamniemiec862@gmail.com>
2023-05-22 20:35:48 +0200

    Add results

--- MapReduce/run.sh
+++ MapReduce/run.sh
@@ -1,6 +1,7 @@
+CMD="hadoop jar /usr/lib/hadoop/hadoop-streaming.jar -files mapper.py,reducer.py -mapper mapper.py -reducer reducer.py -input words -output wordcount-output"
 for size in 1 5 10
 do
-    CMD="time hadoop jar /usr/lib/hadoop/hadoop-streaming.jar -files mapper.py,reducer.py -mapper mapper.py -reducer reducer.py -input s3://aniemiec-bucket/gutenberg-{$size}GB.txt -output wordcount-output"
+    ./get_data.sh $size
     for i in {1..5}
     do
         echo "Running test $i for size $size"
